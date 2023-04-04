abed304a15d3e055f80f83609b54e4a5ad97d2e6
JonathanCecil01 <jonathansesuraj@gmail.com>
2023-04-04 11:53:56 +0530

    Node class created

--- Assets/Scripts/BehaviourTree/Node.cs
+++ Assets/Scripts/BehaviourTree/Node.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Node : MonoBehaviour
+namespace BehaviourTree
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public enum NodeState{
+        RUNNING,
+        SUCCESS,
+        FAILURE
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Node
     {
+        protected NodeState state;
+        public Node parent;
+        protected List<Node> children;
+
+        private Dictionary<string, object> _dataContext = new Dictionary<string, object>();
+        public Node()
+        {
+            parent = null;
+        }
+        public Node (List<Node> children)
+        {
+            children = children;
+        }
+        
+        private void _addChild(Node child)
+        {
+            child.parent = this;
+            children.Add(child);
+        }
+        public virtual NodeState Evaluate() => NodeState.FAILURE; 
+
+        private void SetData(string key, object val){
+            _dataContext[key] = val;
+        }
+
+        public object GetData(string key)
+        {
+            object value = null;
+            if(_dataContext.TryGetValue(key, out value))
+                return value;
+            Node node = parent;
+            while(node!=null)
+            {
+                value = node.GetData(key);
+                if(value!=null)
+                    return value;
+                node= node.parent;
+            }
+            return null;
+        }
+
+        public bool ClearData(string key)
+        {
+            object value = null;
+            if(_dataContext.ContainsKey(key))
+                _dataContext.Remove(key);
+            Node node = parent;
+            while(node!=null)
+            {
+                bool cleared = node.ClearData(key);
+                if(cleared)
+                    return true;
+                node= node.parent;
+            }
+            return false;
+        }
         
     }
 }
+
+
